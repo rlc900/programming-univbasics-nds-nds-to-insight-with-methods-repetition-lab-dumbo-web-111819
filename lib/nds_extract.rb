1374526479126e7cc99004d5fecba88ea32990ed
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-12 23:44:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -42,6 +42,8 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  total = 0 
+  
 end
 
 
