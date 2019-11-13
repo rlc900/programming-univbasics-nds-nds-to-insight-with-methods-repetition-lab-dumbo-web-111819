79d1cdc1e3dbe5b2f0f03b6ea06d8c803e01e5a3
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-13 00:09:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -45,7 +45,7 @@ def total_gross(source)
   total = 0 
   i = 0 
   while i < source.length do 
-    total += directors_totals.values[i]
+    #total += directors_totals.values[i]
     i += 1 
   end 
   total
